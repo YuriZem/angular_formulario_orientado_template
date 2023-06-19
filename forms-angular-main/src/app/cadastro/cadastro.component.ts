2ff88cd9e554967538698917baf89997136de86a
yurizem <yuri.vinicius.dev333@gmail.com>
2023-06-19 11:56:58 -0300

    Finalizando as validacoes utilizando os retornos de uma api de cep adicionando diretivas e preenchimento automatico nos campos

--- forms-angular-main/src/app/cadastro/cadastro.component.ts
+++ forms-angular-main/src/app/cadastro/cadastro.component.ts
@@ -1,3 +1,4 @@
+import { ConsultaCepService } from './../service/consulta-cep.service';
 import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
@@ -9,18 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private consultaCepService:ConsultaCepService
+  ) { }
 
   ngOnInit(): void {
   }
 
   cadastrar(form: NgForm){
-    // if(form.valid){
-    //   this.router.navigate(['./sucesso'])
-    // }else{
-    //   alert('formulario invalido')
-    // }
-      console.log(form);
+    if(form.valid){
+      this.router.navigate(['./sucesso'])
+    }else{
+      alert('formulario invalido')
+    }
+  }
 
+  consultaCep(event:any, f:NgForm){
+    const cep = event.target.value;
+    if(cep!=''){
+      this.consultaCepService.getConsultaCep(cep).subscribe(res => {
+        this.populandoEndereco(res,f)
+      })
+    }
+  }
+  populandoEndereco(dados:any,f:NgForm){
+    console.log(dados)
+    f.form.patchValue({
+      endereco:dados.logradouro,
+      cidade:dados.localidade,
+      uf:dados.uf,
+      bairro:dados.bairro,
+      complemento:dados.complemento
+    })
   }
 }
